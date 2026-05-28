61a5b281a805d948ff282328d571e8f604d899d2
HanJeongmok <wjdahe1845@gmail.com>
2026-05-28 11:38:13 +0900

    캐시적용

--- src/main/kotlin/com/back/demo1/Demo3Application.kt
+++ src/main/kotlin/com/back/demo1/Demo3Application.kt
@@ -2,7 +2,9 @@ package com.back.demo1
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 
+@EnableCaching
 @SpringBootApplication
 class Demo3Application
 
